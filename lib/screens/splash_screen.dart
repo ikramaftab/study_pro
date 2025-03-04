c916215f790c6af421021b56fad210b8f34df2c7
Ikram Aftab <bhattiikram78@gmail.com>
2025-03-04 11:59:31 +0500

    removed start button

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -25,75 +25,77 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          // Background Image
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/background_image.png"),
-                fit: BoxFit.cover,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            // Background Image
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/background_image.png"),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo Animation with Increased Size
-                ZoomIn(
-                  duration: const Duration(seconds: 2),
-                  child: Image.asset(
-                    "assets/images/logo.png",
-                    width: 400, // Bigger logo
-                  ),
-                ),
-                const SizedBox(height: 50),
-                // Animated Button
-                _showButton
-                    ? BounceInUp(
-                  child: GestureDetector(
-                    onTap: () {
-                      print("object");
-                      // Add navigation logic
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 14), // Reduced size
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25), // Adjusted shape
-                        gradient: const LinearGradient(
-                          colors: [Color(0xFF6A11CB), Color(0xFF2575FC)],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 6, // Softer shadow
-                            spreadRadius: 1,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Text(
-                        'Get Started',
-                        style: GoogleFonts.lora(
-                          fontSize: 18, // Adjusted text size
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo Animation with Increased Size
+                  ZoomIn(
+                    duration: const Duration(seconds: 2),
+                    child: Image.asset(
+                      "assets/images/logo.png",
+                      width: 400, // Bigger logo
                     ),
                   ),
-
-                )
-                    : const SizedBox(),
-              ],
+                  const SizedBox(height: 50),
+                  // Animated Button
+                  // _showButton
+                  //     ? BounceInUp(
+                  //   child: GestureDetector(
+                  //     onTap: () {
+                  //       print("object");
+                  //       // Add navigation logic
+                  //     },
+                  //     child: Container(
+                  //       padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 14), // Reduced size
+                  //       decoration: BoxDecoration(
+                  //         borderRadius: BorderRadius.circular(25), // Adjusted shape
+                  //         gradient: const LinearGradient(
+                  //           colors: [Color(0xFF6A11CB), Color(0xFF2575FC)],
+                  //           begin: Alignment.topLeft,
+                  //           end: Alignment.bottomRight,
+                  //         ),
+                  //         boxShadow: const [
+                  //           BoxShadow(
+                  //             color: Colors.black26,
+                  //             blurRadius: 6, // Softer shadow
+                  //             spreadRadius: 1,
+                  //             offset: Offset(0, 3),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //       child: Text(
+                  //         'continue',
+                  //         style: GoogleFonts.lora(
+                  //           fontSize: 18, // Adjusted text size
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Colors.white,
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  //
+                  // )
+                       const SizedBox(),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
